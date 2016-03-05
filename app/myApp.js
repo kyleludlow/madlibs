00485b15bf3ab997904639ecd77506d3fa1d7c86
Kyle Ludlow <kerrymurphy@Kerrys-iMac.local>
2016-03-05 13:28:51 -0800

    Madlibs version 2

--- app/myApp.js
+++ app/myApp.js
@@ -1,15 +1,39 @@
 angular
-    .module('myApp', [])
+    .module('myApp', ['ngMessages'])
     .controller('myCtrl', function ($scope) {
+        $scope.submitForm = function () {
+
+            if ($scope.userInputForm.$valid) {
+                console.log('The form is valid');
+                $scope.display = 1;
+            } else {
+                console.log('The form is not valid');
+            }
+        };
+
         $scope.maleOrFemale = function (gender) {
+
             if (gender === "male") {
-                $('#inputs').show();
                 $('#male-story').show();
                 $('#female-story').hide();
             } else if (gender === "female") {
-                $('#inputs').show();
                 $('#female-story').show();
                 $('#male-story').hide();
             }
-        }
+        };
+        $scope.reset = function () {
+            console.log('Reset!');
+            $scope.display = 0;
+            $scope.name = null;
+            $scope.jobTitle = null;
+            $scope.tediousTask = null;
+            $scope.dirtyTask = null;
+            $scope.celebrity = null;
+            $scope.uselessSkill = null;
+            $scope.obnoxiousCelebrity = null;
+            $scope.hugeNumber = null;
+            $scope.adjective = null;
+        };
+        $scope.display = 0;
+        $('#female-story').hide();
     });
